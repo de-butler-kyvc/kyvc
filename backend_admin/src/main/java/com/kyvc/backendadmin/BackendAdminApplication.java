bbb827015222abef94dd1bdd66629e0693205f41
khuoo <khuoo4603@users.noreply.github.com>
2026-05-01 02:20:02 +0900

    chore(back): 백엔드 및 admin백엔드 프로젝트 초기화

---
+++ backend_admin/src/main/java/com/kyvc/backendadmin/BackendAdminApplication.java
@@ -0,0 +1,12 @@
+package com.kyvc.backendadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAdminApplication.class, args);
+    }
+}
